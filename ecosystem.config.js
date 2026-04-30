0f4c0ea1875149d54321b5aba6b8c07e0e88082e
Richard Salles <richard.danielcs@gmail.com>
2026-04-29 23:26:55 -0300

    removendo cluster do ecosistem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,6 +5,7 @@ module.exports = {
       script: 'dist/index.js',
       cwd: '/www/wwwroot/master-backend',
       instances: 1,
+      exec_mode: 'fork',
       autorestart: true,
       watch: false,
       max_memory_restart: '1G',
