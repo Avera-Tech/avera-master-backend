a586d7191ecf8bbf0fe047373e62f887e3470897
Richard Salles <richard.danielcs@gmail.com>
2026-05-01 22:58:11 -0300

    FEAT: Atualização da versão

--- src/routes/indexRoutes.ts
+++ src/routes/indexRoutes.ts
@@ -5,7 +5,7 @@ const router = Router();
 router.get('/', (_req: Request, res: Response): Response => {
   return res.status(200).json({
     title: 'Avera Backend',
-    version: '1.0.0',
+    version: '1.0.1',
     status: 'online',
     docs: '/api-docs',
   });
